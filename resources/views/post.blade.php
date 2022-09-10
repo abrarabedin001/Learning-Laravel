80b04588b5c77df8c37baee31273cfa5a2da28f7
abrarabedin001 <abrarabedin001@gmail.com>
2022-09-10 17:26:48 +0600

    Made a route and linked it

---
+++ resources/views/post.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="/app.css">
+
+</head>
+<body>
+    <article>
+        <a href="/post"><h1>My first post</h1></a>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto delectus obcaecati animi quibusdam, libero possimus qui eaque quaerat odit reiciendis ullam laboriosam illum, iste accusamus deserunt accusantium unde sed modi eos alias, vero doloremque! Ipsum, non provident error, assumenda praesentium voluptatibus culpa, iure velit beatae obcaecati natus eum magni aspernatur. Alias dolores reprehenderit nulla fugit velit praesentium odit nostrum dolor adipisci quia esse ea tempore accusamus similique repudiandae eius officiis iusto cumque doloribus, laboriosam, autem distinctio quasi natus. Accusamus, fugiat dicta inventore dignissimos ut cum! Eligendi accusantium earum quae asperiores consectetur sequi. Minus aperiam nostrum quasi sed, debitis iusto sapiente?
+        </p>
+    </article>
+    <a href="/">go back</a>
+</body>
+</html>
